eb787c5349466d352713cb3b62c5b98099cda310
Purbohadi Utomo <p.utomo@samsung.com>
2015-09-10 17:01:03 +0700

    Add BestTravelCost Problem
    Find Your Most Best Value Trip

---
+++ src/BestTravelCost.java
@@ -0,0 +1,16 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
+public class BestTravelCost {
+    
+    public static void main(String[] args) throws FileNotFoundException {
+	
+	Scanner sc = new Scanner(new FileInputStream("inputBestTravelCost.txt"));
+	
+	
+	
+    }
+
+}
